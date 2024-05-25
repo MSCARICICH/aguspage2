4300c4b6f41924c42c8c5133c02e388390211310
MSCARICICH <mscaricich@frsn.utn.edu.ar>
2024-05-25 01:24:23 -0300

    agusapge2

---
+++ script.js
@@ -0,0 +1,13 @@
+let nombre="pedro";
+let years=20;
+console.log(years + " " + nombre); 
+let numberone=120;
+let numbertwo=20;
+let resultsuma=numberone + numbertwo;
+let resultresta=numberone - numbertwo;
+let resultmulti= numberone * numbertwo;
+let resultdivision=numberone / numbertwo;
+console.log("suma:", resultsuma)
+console.log("resta", resultresta)
+console.log("multiplicacion", resultmulti)
+console.log("division", resultdivision)
